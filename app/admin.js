76bd084129587eafd95c957ad2f6526977b6f82a
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-08-07 20:14:32 -0300

    se implementa la carga de canciones en local storage

--- app/admin.js
+++ app/admin.js
@@ -1,11 +1,11 @@
 class Cancion {
-  constructor(){
-	this._id;
-    this._titulo;
-    this._artista;
-    this._categoria;
-	  this._imagen;
-    this._duracion;
+  constructor(id){
+	  this._id=id;
+    this._tittle='';
+    this._artist='';
+    this._category='';
+	  this._image='';
+    this._duration='';
   }
   get id(){
     return this._id;
@@ -14,35 +14,52 @@ class Cancion {
     this._id = id;
   }
 	get titulo (){
-		return this._titulo;
+		return this._tittle;
 	}
-	set titulo(titulo){
-		this._titulo = titulo;
+	set titulo(tittle){
+		this._tittle = titulo;
 	}
 	get artista(){
-		return this._artista;
+		return this._artist;
 	}
-	set artista(artista){
-		this._artista = artista;
+	set artista(artist){
+		this._artist = artista;
 	}
 	get categoria(){
-		return this._categoria;
+		return this._category;
 	}
 	set categoria(categoria){
-		this._categoria = categoria;
+		this._category = categoria;
 	}
 	get imagen(){
-		return this._imagen;
+		return this._image;
 	}
 	set imagen(urlImg){
-		this._imagen = urlImg;
+		this._image = urlImg;
 	}
 	get duracion(){
-		return this._duracion;
+		return this._duration;
 	}
 	set duracion(duracion){
-		this._duracion = duracion;
+		this._duration = duracion;
 	}
 } 
+let songIndex = 1;
+let agregarCancion = (event) => {
+  event.preventDefault();
+  let newSong = new Cancion(songIndex);
+  songIndex++;
+  newSong._tittle = document.getElementById('tittleInput').value;
+  newSong._artist = document.getElementById('artistInput').value;
+  newSong._category = document.getElementById('categoryInput').value;
+  newSong._image = document.getElementById('imgInput').value;
+  newSong._duration = document.getElementById('durationInput').value;
 
-let agregarCancion  = ()=>alert("Se agrega cancion");
+	let storedSongs = JSON.parse(localStorage.getItem('songs') || '[]');
+	if(storedSongs.find((song)=>song._tittle===newSong._tittle)){
+		alert('Esta canción ya existe!');
+		return
+	}else storedSongs.push(newSong);
+  localStorage.setItem('songs', JSON.stringify(storedSongs));
+	console.log(JSON.parse(localStorage.getItem('songs')));
+}
