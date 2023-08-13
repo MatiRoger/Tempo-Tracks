7f02485051998a93b520d556e91b60b3025f740d
santi0dino <serodinosantiago765@gmail.com>
2023-08-13 03:23:36 -0300

    se realizaron cambios y se termino por completo la pagina de registro

--- app/registerPage.js
+++ app/registerPage.js
@@ -1,53 +1,109 @@
+const closeIcon = document.getElementById("eyeClose");
+const openIcon = document.getElementById("eyeOpen");
+const closeIcon2 = document.getElementById("eyeClose2");
+const openIcon2 = document.getElementById("eyeOpen2");
+const userPassword = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+const showPassword = () => {
+
+    if (userPassword.type === "password"){
+        userPassword.type = "text";
+        openIcon.style.display = "inline-block";
+        closeIcon.style.display = "none";
+    } else {
+        userPassword.type = "password"
+        openIcon.style.display = "none";
+        closeIcon.style.display = "inline-block";
+    }
+};
+
+const showPassword2 = () => {
+
+    if (confirmPassword.type === "password"){
+        confirmPassword.type = "text";
+        openIcon2.style.display = "inline-block";
+        closeIcon2.style.display = "none";
+    } else {
+        confirmPassword.type = "password"
+        openIcon2.style.display = "none";
+        closeIcon2.style.display = "inline-block";
+    }
+};
+
 const createUser = (event) => {
     event.preventDefault();
-    const userEmail = document.getElementById("email").value;
-    const userName = document.getElementById("name").value.toLowerCase();
-    const userLastName = document.getElementById("lastName").value.toLowerCase();
-    const userPassword = document.getElementById("password").value;
+
+    const newUser = {
+        email: document.getElementById("email").value,
+        name: document.getElementById("name").value.toLowerCase(),
+        lastName: document.getElementById("lastName").value.toLowerCase(),
+        password: document.getElementById("password").value,
+    };
+
+    const errores = {
+        errorPassword: document.getElementById("errorPassword"),
+        errorConfirmPassword: document.getElementById("errorConfirmPassword"),
+        errorName: document.getElementById("errorName"),
+        errorLastName: document.getElementById("errorLastName"),
+        errorConditions: document.getElementById("errorConditions")
+    }
+
     const confirmPassword = document.getElementById("confirmPassword").value;
     const conditions = document.getElementById("conditions").checked;
-
     const emailRegex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const passwordRegex =  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_])(?!.*\s).{8,}$/;
 
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
-
-    if (userName.length < 3) {
-        alert("ingrese un nombre valido");
-        return;
+    if (newUser.name.length < 3) {
+        errores.errorName.style.display = "block"
+    } else {
+        errores.errorName.style.display = "none"
     }
 
-    if (userLastName.length < 3) {
-        alert("ingrese un apellido valido");
-        return;
+    if (newUser.lastName.length < 3) {
+        errores.errorLastName.style.display = "block"
+    } else {
+        errores.errorLastName.style.display = "none"
     }
 
-    if (!emailRegex.test(userEmail)) {
+    if (!emailRegex.test(newUser.email)) {
         alert("debe ingresar un email valido")
         return;
     }
 
-    if (!passwordRegex.test(userPassword)) {
-        alert("debe ingresar una contraseña valida")
-        return;
+    if (!passwordRegex.test(newUser.password)) {
+        errores.errorPassword.style.display = "block"
+    } else {
+        errores.errorPassword.style.display = "none"
     }
 
-    if (userPassword !== confirmPassword) {
-        alert("Las contraseñas no coinciden")
-        return;
+    if (newUser.password !== confirmPassword) {
+        errores.errorConfirmPassword.style.display = "block"
+    } else {
+        errores.errorConfirmPassword.style.display = "none"
     }
 
     if (!conditions) {
-        alert("Debes aceptar nuestros terminos y condiciones")
+        errores.errorConditions.style.display = "block"
+        return
     }
 
-    const newUser = {
-        email: userEmail,
-        name: userName,
-        lastName: userLastName,
-        password: userPassword,
-    };
+    const storageUsers = JSON.parse(localStorage.getItem("users")) || [];
+    
+    const userAlreadyExist = storageUsers.find((user) =>{
+        return user.email === newUser.email
+    });
 
-    localStorage.setItem("user", JSON.stringify(newUser));
+    let users = storageUsers;
 
-    window.location.href = "#"
+    if (userAlreadyExist) {
+        alert("Ya existe un usuario registrado con este email, por favor ingrese otro.")
+        return;
+    } else {
+        users.push(newUser)
+    }
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+     window.location.href = "http://127.0.0.1:5500/pages/loginPage.html"
 }
