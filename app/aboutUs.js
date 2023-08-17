8ec6fb7fcf970ce11f54f5185dba0e7ab3e8d3f8
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-08-17 17:14:04 -0300

    Se incorpora seccion sobre nosotros

---
+++ app/aboutUs.js
@@ -0,0 +1,45 @@
+const section = document.querySelector('.member-card-section');
+const members = [
+  {
+    name:'Matias Roger Narcotti',
+    pic:'../assets/Mati.png',
+    description:'28 años, estudio profesorado de Inglés y tengo mucho interés en el desarrollo backend',
+    gitHub:'https://github.com/MatiRoger',
+    linkedIn:'https://www.linkedin.com/in/matias-roger-narcotti-dev/'
+  },
+  {
+    name:'Santiago Serodino',
+    pic:'../assets/Santi.png',
+    description:'18 años, futuro programador e Ing. En Sistemas (y también alumno destacado)',
+    gitHub:'https://github.com/santi0dino',
+    linkedIn:''
+  },
+  {
+    name:'Gonzalo Cainzo',
+    pic:'../assets/Gonza.png',
+    description:'30 años, comerciante, estudiante de programación e Inglés. Me gusta entrenar y los deportes.',
+    gitHub:'https://github.com/cainzogonzalo',
+    linkedIn:''
+  }
+];
+
+members.forEach(member => {
+  const card = document.createElement('div');
+	card.classList.add('member-card',  'col-11','col-md-6', 'col-lg-3');
+	card.innerHTML = `<div class="member-card-content container row w-100 p-0 m-0">
+	<div class="member-picture col-4" style="background-image:url(${member.pic});"></div>
+		<div class="member-info col-8">
+			<h4>${member.name}</h4>
+			<p>${member.description}</p>
+			<div class="container-fluid row justify-content-evenly w-100 mt-1">
+				<a class="admin-option aside-buttons text-center" href="${member.gitHub}">            
+					<i class="bi bi-github"></i>
+				</a>
+				<a class="admin-option aside-buttons text-center" href="${member.linkedIn}">            
+					<i class="bi bi-linkedin"></i>
+				</a>
+			</div>
+		</div>
+	</div>`
+	section.appendChild(card);
+});
